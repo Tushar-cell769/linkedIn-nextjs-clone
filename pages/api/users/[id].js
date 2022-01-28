df6fbbf298716af43650c95849c9cc20d4c17559
Tushar <rajtushar33@gmail.com>
2022-01-29 02:14:27 +0530

    Fixed api issue for updating various fields.

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -9,12 +9,17 @@ export default async function handler(req, res) {
   } = req;
 
   const { db } = await connectToDatabase();
+  const toUpdate = body.toUpdate;
+  const exactBody = body.data;
 
   if (method === "PUT") {
     try {
       const user = await db
         .collection("users")
-        .updateOne({ _id: ObjectId(id) }, { $set: { metadata: body } });
+        .updateOne(
+          { _id: ObjectId(id) },
+          { $set: { [`metadata.${toUpdate}`]: exactBody } }
+        );
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json(error);
