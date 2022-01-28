3c5e38d510257725d76a33fcc79fd9e321df53f0
Tushar <rajtushar33@gmail.com>
2022-01-29 01:05:09 +0530

    Add progress bar at the top of page.

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,19 @@ import { ThemeProvider } from "next-themes";
 import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
 
+import ProgressBar from "@badrap/bar-of-progress";
+import { Router } from "next/router";
+const progress = new ProgressBar({
+  size: 4,
+  color: "#0a66c2",
+  className: "z-50",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
